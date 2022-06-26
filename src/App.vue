15f871868ebb44a9852ddea4afe48553d2e58bc1
zjh <zjiahe0124@163.com>
2022-06-26 17:47:09 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <MyHeader></MyHeader>
+    <div class="main"><MyGoods></MyGoods></div>
+    <MyFooter></MyFooter>
+  </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader'
+import MyGoods from './components/MyGoods'
+import MyFooter from './components/MyFooter'
 export default {
-
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.mian {
+  padding-top: 45px;
+  padding-bottom: 50px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css' // 引入第三方包
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
