0ffd3280bf5f3f866ce73a009d020b3ae27c5f6a
zjh <zjiahe0124@163.com>
2022-06-26 17:31:54 +0800

    初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+// defineConfig:提示语法
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭eslint, build serve将不会进行eslint检查
+  lintOnSave: false, // 关闭语法检查
+
+  // 写原生webpack的配置项
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 3000,
+      host: 'localhost'
+    }
+  }
 })
